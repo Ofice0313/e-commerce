9279bd18138b483c63d5663dc35d666db1719025
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-03-18 05:34:45 +0200

    Project created and User class

---
+++ src/test/java/com/devcaleb/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcaleb.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
