ba27735a26a5ae7d3c2112b473a992b6bd2e971e
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-04-05 11:45:08 +0200

    ProductController, ProductService, ProductDTO and Get Product by Id

---
+++ src/main/java/com/devcaleb/ecommerce/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.devcaleb.ecommerce.services;
+
+import com.devcaleb.ecommerce.dto.ProductDTO;
+import com.devcaleb.ecommerce.entities.Product;
+import com.devcaleb.ecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional
+    public ProductDTO findById(Long id){
+        Optional<Product> result = repository.findById(id);
+        Product product = result.get();
+        ProductDTO dto = new ProductDTO(product);
+        return dto;
+    }
+}
